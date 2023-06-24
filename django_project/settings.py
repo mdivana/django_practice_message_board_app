efc5989d191982c34c7317e85f73d7d5993707cf
mdivana <nikamdivani@icloud.com>
2023-06-24 14:18:23 +0400

    added some files for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zo!d-l!(8c)bjy*f-op377m(w%b+kn!kixvqny)04otpz%ijqy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
